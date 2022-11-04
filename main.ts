cb44fbbb461fe1c988fccbafdbd1f1abc8e8b275
abarrows292 <117087235+abarrows292@users.noreply.github.com>
2022-11-04 11:52:59 -0400

    finished

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy()
     head.sayText("yum")
-    info.changeScoreBy(1)
+    info.changeScoreBy(2)
 })
 let head: Sprite = null
 game.splash("hungry shark")
 info.setScore(0)
-info.startCountdown(5)
+info.startCountdown(10)
 info.setLife(3)
 let food = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -80,15 +80,41 @@ let otherFish = sprites.create(img`
     . . . . . . f f f f d d d c . . 
     . . . . . . . . . . c c c . . . 
     `, SpriteKind.Enemy)
+let fishy = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Enemy)
 food.setStayInScreen(true)
 head.setStayInScreen(true)
 fish.setStayInScreen(true)
 otherFish.setStayInScreen(true)
+fishy.setStayInScreen(true)
 food.setPosition(randint(0, 10), randint(0, 10))
 fish.setPosition(randint(0, 100), randint(0, 100))
 otherFish.setPosition(randint(0, 100), randint(0, 100))
+fishy.setPosition(randint(0, 100), randint(0, 100))
 head.setPosition(120, 60)
 game.onUpdate(function () {
     head.x += controller.dx()
     head.y += controller.dy()
+})
+game.onUpdateInterval(300, function () {
+    food.setPosition(randint(0, 10), randint(0, 10))
+    fish.setPosition(randint(0, 100), randint(0, 100))
+    otherFish.setPosition(randint(0, 100), randint(0, 100))
+    fishy.setPosition(randint(0, 100), randint(0, 100))
 })
