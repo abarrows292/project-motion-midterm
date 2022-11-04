3fb6826c34230fffa525eff3827f32ae0b076a0e
abarrows292 <117087235+abarrows292@users.noreply.github.com>
2022-11-04 11:48:37 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,94 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    head.sayText("yum")
+    info.changeScoreBy(1)
+})
+let head: Sprite = null
+game.splash("hungry shark")
+info.setScore(0)
+info.startCountdown(5)
+info.setLife(3)
+let food = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Enemy)
+head = sprites.create(img`
+    ...........fffffff...ccfff..........
+    ..........fbbbbbbbffcbbbbf..........
+    ..........fbb111bbbbbffbf...........
+    ..........fb11111ffbbbbff...........
+    ..........f1cccc1ffbbbbbcff.........
+    ..........ffc1c1c1bbcbcbcccf........
+    ...........fcc3331bbbcbcbcccf..ccccc
+    ............c333c1bbbcbcbccccfcddbbc
+    ............c333c1bbbbbbbcccccddbcc.
+    ............c333c11bbbbbccccccbbcc..
+    ...........cc331c11bbbbccccccfbccf..
+    ...........cc13c11cbbbcccccbbcfccf..
+    ...........c111111cbbbfdddddc.fbbcf.
+    ............cc1111fbdbbfdddc...fbbf.
+    ..............cccfffbdbbfcc.....fbbf
+    ....................fffff........fff
+    `, SpriteKind.Player)
+let fish = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Enemy)
+let otherFish = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Enemy)
+food.setStayInScreen(true)
+head.setStayInScreen(true)
+fish.setStayInScreen(true)
+otherFish.setStayInScreen(true)
+food.setPosition(randint(0, 10), randint(0, 10))
+fish.setPosition(randint(0, 100), randint(0, 100))
+otherFish.setPosition(randint(0, 100), randint(0, 100))
+head.setPosition(120, 60)
+game.onUpdate(function () {
+    head.x += controller.dx()
+    head.y += controller.dy()
+})
